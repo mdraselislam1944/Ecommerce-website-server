e9780a1e7049bcf467c1809f9bb5f88fa26c05cf
mdraselislam1944 <mdraselislam1944@gmail.com>
2023-06-24 13:09:32 +0600

    'add-product'

--- index.js
+++ index.js
@@ -24,15 +24,58 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const products = client.db('emaJohnDB').collection('products');
+    const users = client.db('emaJohnDB').collection('users');
+    const userProducts=client.db('emaJohnDB').collection('userProducts')
     app.get('/products', async (req, res) => {
       const result = await products.find().toArray();
       res.send(result);
     })
     
+    app.get('/products/:category', async (req, res) => {
+      const category = req.params.category;
+      console.log(category);
+      const query = { category: category };
+      const result = await products.find(query).toArray();
+      res.send(result);
+    })
+
     app.get('/products/:id', async (req, res) => {
       const id = req.params.id;
       const result = await products.findOne({ _id:new ObjectId(id) });
       res.send(result);
+  })
+  app.post('/products',async(req,res)=>{
+    const product=req.body;
+    const result= await products.insertOne(product);
+    res.send(result);
+  })
+  app.post('/users',async(req,res)=>{
+    const user = req.body;
+      const result = await users.insertOne(user);
+      res.send(result);
+  })
+
+  app.get('/users',async(req,res)=>{
+    const result=await users.find().toArray();
+    res.send(result);
+  })
+
+
+  app.post('/userProducts',async(req,res)=>{
+    const product=req.body;
+    const result= await userProducts.insertOne(product);
+    res.send(result);
+  })
+  app.get('/userProducts',async(req,res)=>{
+    const result=await userProducts.find().toArray();
+    res.send(result);
+  })
+
+  app.delete('/userProducts/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id:new ObjectId(id)}
+    const result=await userProducts.deleteOne(query);
+    res.send(result);
   })
 
     await client.db("admin").command({ ping: 1 });
